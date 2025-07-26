0635e2364bcb58349370508e061a649ae289a2c1
b22thohe <tomhell66@gmail.com>
2025-07-26 11:48:52 +0200

    Initial commit

---
+++ src/main/java/com/example/myurl/MyurlApplication.java
@@ -0,0 +1,13 @@
+package com.example.myurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myurl/MyurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
