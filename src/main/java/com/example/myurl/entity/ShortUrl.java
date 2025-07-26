af572ecd227286cf9240008a0175602e3db5b870
b22thohe <tomhell66@gmail.com>
2025-07-26 13:20:04 +0200

    Add Entity class

---
+++ src/main/java/com/example/myurl/entity/ShortUrl.java
@@ -0,0 +1,41 @@
+package com.example.myurl.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+
+    private int clickCount;
+
+    // Constructor
+    public ShortUrl() {}
+
+    //getters, and setters
+    public ShortUrl(String originalUrl, String shortCode) {
+        this.originalUrl = originalUrl;
+        this.shortCode = shortCode;
+        this.createdAt = LocalDateTime.now();
+        this.clickCount = 0;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
